e943a94c334289bc5a908da3eff4455f0ee24ca1
David Nguyen <dave_n998@hotmail.com>
2020-02-17 20:21:47 +0000

    Learning operators and basic syntax rules, coding challenge 1

--- tutorial1/script.js
+++ tutorial1/script.js
@@ -77,9 +77,39 @@ console.log(typeof x);
 
 /**
  * Operator precedence
+ * Precedence is priority of what is executed first
  */
 
- 
+var now = 2018;
+var yearJohn = 1989; 
+var fullAge = 18;
+
+// multiple operators
+var isFullAge = now - yearJohn >= fullAge; //Left to right
+console.log(isFullAge);
+
+// grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2; //Left to right
+console.log(average);
+
+// multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; //8 * 4 - 6 
+console.log(x,y);
+
+// more operators
+x *= 2;
+console.log(x);
+
+x *= 10;
+console.log(x);
+
+x--;
+console.log(x);
+
+
 
 
 
