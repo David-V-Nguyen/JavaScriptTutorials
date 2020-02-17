2a9654985b0557995c9e739cbecee87911e17d09
David Nguyen <dave_n998@hotmail.com>
2020-02-17 23:00:41 +0000

    Created switch statements and ternary operators

--- tutorial1/script.js
+++ tutorial1/script.js
@@ -144,7 +144,7 @@ console.log("Is Mark's BMI lower than John's? " + isMarksBMILower);
 /**
  * IF/Else statements
  */
-
+/*
 var firstName = "john";
 var civilStatus = "single";
 
@@ -183,7 +183,79 @@ if (markBMI < johnBMI){
 /**
  * Boolean Logic 
  */
+/*
+var firstName = "John";
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + " is a boy.");
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + " is a teenager.");
+} else {
+    console.log(firstName + " is a man.");
+}
+
+/**
+ * Tenary operator and switch statements
+ */
+/*
+var firstName = "John";
+var age = 22;
+
+// Ternary operator
+age >= 18 ? console.log(firstName + " drinks beer.") 
+: console.log(firstName + " drinks juice.");         
+// age >= 18 (condition)
+// ? console.log(drinks beer) (IF statement)
+// : console.log(drinks juice)(Else statement)
+
+var drink = age >= 18 ? "beer" : "juice";   //age >= 18 ? "beer" : "juice" (operator)
+console.log(drink);
+
+if (age >= 18){
+    var drink = "beer";
+} else {
+    var drink = "juice";
+}
+*/
+// switch statement
+
+var firstName = "John";
+var job = "teacher";
+switch(job) {
+    case "teacher":
+    case "instructor":
+        console.log(firstName + " teaches kids how to code.");
+        break;
+    case "driver":
+        console.log(firstName + " drivers an uber in Lisbon");
+        break;
+    case "designer":
+        console.log(firstName + " designs beautiful websites");
+        break;
+    default:
+        console.log(firstName + " does something else.");
+}
+
+var firstName = "John";
+var age = 20;
+
+switch(true) {
+    case age < 13:
+        console.log(firstName + " is a boy");
+        break;
+    case age > 13 && age < 20:
+        console.log(firstName + " is a teenager");
+        break;
+    default:
+        console.log(firstName + " is a man");
+}
+
+/**
+ * Truthy and Falsy values and Equality operators
+ */
 
 
 
- 
+
+
