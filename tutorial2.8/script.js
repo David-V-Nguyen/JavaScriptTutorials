78346f97b79d9fd45324f0da57a7d7d5abb41c52
David Nguyen <dave_n998@hotmail.com>
2021-05-18 18:30:37 +0100

    added index.html file resources and learnt strict mode for JS

---
+++ tutorial2.8/script.js
@@ -0,0 +1,8 @@
+'use strict';
+// shows visible errors in the console
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive now');
